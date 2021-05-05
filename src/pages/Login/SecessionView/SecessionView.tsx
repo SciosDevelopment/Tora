d273db9cc8946ffb173e6765daeb95d301a4d7ff
Nokda <yh1483@naver.com>
2021-05-06 00:51:27 +0900

    #feat : secession 기능 구현 1. 유효성검사 구현  - 로그인이 되어있을 때만 접근할 수 있도록 useEffect에 추가  - confirm message가 일치하는지 확인
    2. sesession 기능 구현
     - axios를 이용해 서버에 사용자정보를 제거할 것을 요청 (유효성검사 후)
     - 탈퇴 성공 시, 클라이언트에 있는 사용자정보를 모두 제거하고, 메인으로 이동
     - 탈퇴 실패 시, 에러 상태에 대한 에러 처리 (임시)
    
    * 하드웨어 이슈로 인한 테스트 이전에 미리 커밋
    
    #5

--- src/pages/Login/SecessionView/SecessionView.tsx
+++ src/pages/Login/SecessionView/SecessionView.tsx
@@ -1,6 +1,79 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 import './style/SecessionView.scss'
-const SecessionView = () => {
+import useUser from '../../../hooks/useUser'
+import {history} from 'src/configureStore'
+import {useCookies} from 'react-cookie'
+
+const SecessionView:React.FC = () => {
+    const [inputs, setInputs] = useState({userID:"", password:"", confirmmessage:""})
+    const {onSetUserInfo, onGetUserInfo} = useUser()
+    const [cookies, _, removeCookie] = useCookies(['ToraLoginToken', 'ToraID'])
+
+    useEffect(()=>{
+        // 로그인이 되어있지 않다면, 메인으로 이동
+        if(onGetUserInfo === null) {
+            history.replace("/")
+            return
+        }
+        setInputs({...inputs, userID:onGetUserInfo.toString()})
+    },[])
+
+    const logout = () => {
+        // remove client data
+        onSetUserInfo("")
+        removeCookie('ToraLoginToken')
+        removeCookie('ToraID')
+        axios.defaults.headers.common['Authorization'] = null
+    }
+
+    const secession = async() => {
+        // 204: 성공
+        // 400: 오류
+        // 404: 유저를 찾을 수 없음
+        const data = {user:{id:inputs.userID, password:inputs.password}}
+        axios.put("/api/v1/user/secession", data)
+        .then((res)=>{ // 204
+            alert("success secession")
+            logout()
+        })
+        .catch((e)=>{
+            // error : status is not defined.
+            if(e.response) {
+                var status = e.response.status // or use message
+                // 이미 등록된 사용자일때 : temp-status
+                if(status === 404) {
+                    alert("already seceded user")
+                    logout()
+                }
+
+                if(status === 400) alert(e)
+                
+                // 서버 연결 문제일때 : temp-status
+                if(status >= 500) alert("server is dead")
+            }
+            else if(e.request) {
+                alert("internal server error")
+                console.log(e.request)
+            }
+            else console.log('Error', e.message)
+        })
+    }
+    
+    const handleChange = (e) => {
+        const{target : {name, value}} = e
+        setInputs({...inputs, [name]:value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(inputs.confirmmessage !== "delete account") {
+            alert("incorrect confirm message")
+            return
+        }
+        secession()
+    }
+
     return (
         <div className = "Secess-account-view">
             <div className = "Secess-account-title">
@@ -18,16 +91,15 @@ const SecessionView = () => {
 
             </div>
 
-            <form className = "Secess-account-input-form">
+            <form className = "Secess-account-input-form" onSubmit ={handleSubmit}>
                 <div className = "Secess-account-input-wrapper">
                     {
-                        `type in your username/email to 
+                        `type in your password to 
                         procede to delete account`
                     }
                      <div className = "Secess-account-input-container">
-                        <input type = "input" required/>
+                        <input type = "input" name="password" required onChange={handleChange}/>
                     </div>
-                    
                 </div>
                 
                 <div className = "Secess-account-input-wrapper">
@@ -36,7 +108,7 @@ const SecessionView = () => {
                         to finish deleting your account`
                     }
                       <div className = "Secess-account-input-container">
-                        <input type = "input" required/>
+                        <input type = "input" name="confirmmessage" required onChange={handleChange}/>
                     </div>
                 </div>
                 
