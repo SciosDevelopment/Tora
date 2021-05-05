a658359502e081e853c3d3521c16fab82e0821e2
Nokda <yh1483@naver.com>
2021-05-05 15:55:51 +0900

    # feat : reset_pw 기능 구현
    1. 유효성검사 추가
     - 이메일 형식이 맞는지 확인
    
    2. sendMail 기능 구현
     - 이메일 형식이 맞다면, axios를 이용해 서버에 메일전송을 요청
     - 이메일이 DB에 존재한다면 전송되었음을 UI에 표시
     - 전송되지 않았을 경우, alert를 이용해 표시 (임시)
    
    * 하드웨어 이슈로 인한 테스트 이전에 미리 커밋
    
    #4

--- src/pages/Login/ResetPasswordView/ResetPasswordView.tsx
+++ src/pages/Login/ResetPasswordView/ResetPasswordView.tsx
@@ -1,41 +1,75 @@
+import axios from 'axios'
 import React, { useState } from 'react'
+import { history } from '../../../configureStore'
 import './style/ResetPasswordView.scss'
 
 const ResetPasswordView:React.FC = () => {
-    const [checkEmail, setCheckEmail] = useState<boolean>(false);
-    const [email, setEmail] = useState<string>("example@email.com");
+    const [checkEmail, setCheckEmail] = useState<boolean>(false)
+    const [emailinfo, setEmailinfo] = useState<string>("example@email.com")
+    
+    // email 유효성 검사
     const checkEmailStatus = () => {
-        //server connect-> if data === true;
-        //
+        const regex_email = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i 
+        
+        if(emailinfo.match(regex_email)){
+            setCheckEmail(true) 
+            // send email to 'email'
+            sendMail()        
+        }
+        else{
+            alert("email 형식이 다릅니다.")
+            setEmailinfo("")
+        } 
     }
+
+    const sendMail = async() => {
+        // send to email
+        const data = {user:{email:emailinfo}}
+        axios.put("/api/v1/user/send_reset_password_email", data)
+        .then((res)=>{
+            alert("send reset password email to " + emailinfo)
+            history.replace("/")
+        })
+        .catch((e)=>{
+            // temp status
+            alert("internal server error")
+        })
+    }
+    
+    const handleChange = (e) => {
+        const{target : {_, value}} = e
+        setEmailinfo(value)
+    }
+
     return (
         <div className = "Reset-password-view-main">
             <div className = "Reset-password-title-container">
                 {`Reset Your Password`}
             </div>
             
-            <form className = "Reset-password-view-form">
+            <div className = "Reset-password-view-form">
                 <div className = "Reset-password-view-title">
                     <p>type in your username / email</p>
                 </div>
 
-                <div className = "Reset-password-view-input-container">
-                    <input/>
-                </div>
-               
-                <div className = "Reset-password-view-alert-text">
-                    {`your temporary password
-                    has been sent to your account
-                    [ ${email} ]`}
-                </div>
+                { checkEmail !== true ?
+                    <div className = "Reset-password-view-input-container">
+                        <input onChange={handleChange}/>
+                    </div>
+                :
+                    <div className = "Reset-password-view-alert-text">
+                        your temporary password
+                        has been sent to your account
+                        [ {emailinfo} ]
+                    </div>
+                }
 
                 <div className = "Reset-password-view-submit-button">
-                    <button onClick = {()=>checkEmailStatus}>Reset Password</button>
+                    <button onClick = {checkEmailStatus}>Reset Password</button>
                 </div>
-            </form>
-
+            </div>
         </div>
-    );
-};
+    )
+}
 
 export default ResetPasswordView
