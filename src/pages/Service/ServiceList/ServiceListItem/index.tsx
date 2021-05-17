cace9381505ea5c8bfe765f0bea91409f4717af8
taewook1 <aktw96@naver.com>
2021-05-17 14:30:40 +0900

    #refactor: ServiceListitem, Servicelist 페이지 수정 기능 추가 Comment 간격 수정
    1. ServiceList 더보기 버튼 추가
     - 더보기 화살표 추가 누를시 detail 페이지 부분을 보여줌
    
    2. ServiceListitem 새로 구성
     - modify 버튼 부분 추가
     - 기존에 있던 detail페이지 부분을 가져온것이 아닌 컴포넌트를 가져와 새로 구성
    
    3. Comment 간격 수정
     - 날짜와 프로필 사이 간격 축소
     - 한 줄에 맞게 사이즈 조정

--- src/pages/Service/ServiceList/ServiceListItem/index.tsx
+++ src/pages/Service/ServiceList/ServiceListItem/index.tsx
@@ -1,15 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './style/ServiceListItem.scss'
+import Comment from '../../../../../src/components/common/Comment'
+import CommentPost from '../../../../../src/components/common/CommentPost'
+import modify from '../../../../img/modify.png'
 
-const ServiceListItem = () => {
+const ServiceListItem:React.FC = () => {
+    const [openStatus, setOpenStatus] = useState<Boolean>(false);
+
     return (
         <div className = "Service-listitem-main">
-            <div className = "Service-listitem-text">
-                <p>What types of questions should I avoid asking?</p>
+            <div className = "Service-listitem-list">
+                <div className = "Service-listitem-text">
+                    <p>What types of questions should I avoid asking?</p>
+                </div>
+                <div className = "Service-listitem-date">
+                    <p>2020.10.23</p>
+                </div>   
+                <div className = "Service-listitem-detail-button" onClick = {()=>setOpenStatus(!openStatus)}>
+                    <div className = "Service-listitem-arrow">
+
+                    </div>
+                </div>
             </div>
-            <div className = "Service-listitem-date">
-                <p>2020.10.23</p>
-            </div>            
+
+            <div className = "Service-listitem-detail">
+                {
+                    openStatus === false ? 
+                    
+                    <div className = "Service-listitem-closed">
+                        
+                    </div>
+                :
+                    <div className = "Service-listitem-open">
+                        <div className = "Service-listitem-modify">
+                            <img src ={modify}/>
+                        </div>
+                        <div className = "Service-listitem-text-wrapper">
+                                <p>How do I write a good answer?</p>
+                                <p>How do I write a good answer?</p>
+                                <p>How do I write a good answer?</p>
+                                <p>How do I write a good answer?</p>
+                                <p>How do I write a good answer?</p>
+                        </div>
+                        <div className = "Service-listitem-comment-wrapper">
+                            <Comment/>
+                        </div>
+                        <div className = "Service-listitem-commentpost-wrapper">
+                            <CommentPost/>
+                        </div>
+                    </div> 
+                }
+            </div>
         </div>
     )
 }
