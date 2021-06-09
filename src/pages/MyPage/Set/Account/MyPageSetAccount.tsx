b003ca5d4df02308a2d0072c8ad8bf14a5774d7e
taewook1 <aktw96@naver.com>
2021-06-09 13:30:42 +0900

    #refactor : MyPage/set 수정 snsaccount 컴포넌트 생성
    1. MyPage/set 디자인 변경 구성 변경, sns accounts 생성
     - 간격 조정
     - 색상 변경
     - border-radius 수정
     - 버튼 크기 padding값 간격 조정
     - SNS accounts 컴포넌트 생성 후 추가

--- src/pages/MyPage/Set/Account/MyPageSetAccount.tsx
+++ src/pages/MyPage/Set/Account/MyPageSetAccount.tsx
@@ -2,33 +2,39 @@ import React from 'react';
 import Title from 'src/components/common/Title/Title';
 import './style/MyPageSetAccount.scss'
 import Profile from '../../../../img/profile4.png'
+import Plus from '../../../../img/plus-gray.png'
+import Minus from '../../../../img/minus-gray.png'
+import SNSaccounts from './SNSaccounts/SnsAccounts'
 
 const MyPageSetAccount = () => {
     return (
         <div className = "Mypage-set-account-main">
             <div className = "Mypage-set-account-profile">
                 <Title name = "Profile"/>
-                <img src = {Profile}/>
-                <div className = "Mypage-set-account-profile-text">
-                    <p>이미지 업로드</p>
-                    <p>이미지 삭제</p>
+                <div className = "Mypage-set-account-profile-img">
+                    <img src = {Profile}/>
+                    <div className = "Mypage-set-account-profile-set">
+                        <img src = {Plus}/>
+                        <img src = {Minus}/>
+                    </div>
                 </div>
-            </div>
-            <div className = "Mypage-set-account-name">
-                <Title name = "Name"/>
-                <p>james</p>
             </div>
             <div className = "Mypage-set-account-region">
                 <Title name = "Region"/>
                 <p>Region</p>
             </div>
+            <div className = "Mypage-set-account-name">
+                <Title name = "Name"/>
+                <p>james</p>
+            </div>
+
             <div className = "Mypage-set-account-password">
-                <Title name = "Password"/>
+                <Title name = "Confirm Password"/>
                 <p>asdasdasdasd</p>
             </div>
             <div className = "Mypage-set-account-description">
                 <Title name = "Description"/>
-                <p>gkernjoidsahjfioajed@naver.comgkernjoidsahjfioajed@naver.com</p>
+                <p>gkernjoidsahjfioajed@naver.comgkernjoidsahjfioajed@naver.comasdasdasdasdasdsadasd</p>
             </div>
             <div className = "Mypage-set-account-program-language">
                 <Title name = "Program Language"/>
@@ -40,15 +46,7 @@ const MyPageSetAccount = () => {
             </div>
             <div className = "Mypage-set-account-sns-accounts">
                 <Title name = "SNS accounts"/>
-                <div className = "Mypage-set-account-sns-first">
-
-                </div>
-                <div className = "Mypage-set-account-sns-second">
-
-                </div>
-                <div className = "Mypage-set-account-sns-third">
-
-                </div>
+                <SNSaccounts/>
             </div>
             <div className = "Mypage-set-account-save-button">
                 <p>Save Changes</p>
