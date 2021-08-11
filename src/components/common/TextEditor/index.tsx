09abecae4793c0f70e1983a6a66bdc7e7ca90e1b
Nokda <yh1483@naver.com>
2021-08-11 21:19:01 +0900

    # feat : TextEditor CustomContextMenu 구현
    - setContextMenu를 통해서 CustomContextMenu를 구현
    - 기본 ContextMenu를 제거하고 CustomContextMenu하는 방법을 찾아봐야함
    
    ※ ContextMenu에 들어갈 내용 논의 필요
    
    #20

--- src/components/common/TextEditor/index.tsx
+++ src/components/common/TextEditor/index.tsx
@@ -1,21 +1,54 @@
 import React from 'react'
-import MonacoEditor from 'react-monaco-editor'
+import { useEffect } from 'react'
+import MonacoEditor, {monaco} from 'react-monaco-editor'
 
 const TextEditor = (props) => {
     // https://github.com/react-monaco-editor/react-monaco-editor
     const {code, File} = props
+    const ref = React.createRef<MonacoEditor>()
+    useEffect(()=>{
+        init()
+    },[])
+    
+    const init = () => {
+        setContextMenu(ref)
+    }
+
+    const setContextMenu = (ref) => {
+       // ※ 코드 수정 후 새로고침시 여러번 등록될 수 있음. 실제 프로그램에선 문제없음.
+       // 코드 수정 후 저장 + url로 reload하면 해결가능
+        ref.current.editor.addAction({ // 예시 코드
+            id: "testAlert",
+            label: "testAlert",
+            contextMenuGroupId: "0_testAlert", // number = order
+
+            run: () => {
+                alert("TestAlert")
+          }
+        })
+    }
     
     const options = {
         selectOnLineNumbers: false,
-        automaticLayout: true
+        automaticLayout: true,
+        setContextMenu: false
     }
     return ( 
         <MonacoEditor
         language="javascript"
         theme="vs-dark"
         value={code}
-        options={options}/>
+        options={options}
+        ref={ref}/>
     )
 }
 
 export default TextEditor
+
+    /* how to get whole actions in monaco-editor?
+        => 참고 : https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.ieditor.html 
+        => 참고 : https://stackoverflow.com/questions/62001650/how-to-open-context-menu-in-monaco-editor-based-on-line-content-in-angular
+    */ 
+
+    // ref.addaction 참고 : https://stackoverflow.com/questions/48745208/disable-cut-and-copy-in-context-menu-in-monaco-editor
+    // custom auto-complete : https://stackoverflow.com/questions/38735644/how-to-add-auto-completion-list-in-monaco
