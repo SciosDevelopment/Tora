f7923842a2b96d8c54aa4d20b594051880e689e2
taewook1 <aktw96@naver.com>
2021-08-11 18:35:23 +0900

    # feat : sharemodal 컴포넌트 추가,  star button toggle기능 추가, follow button toggle 기능 추가, save 버튼 axios 데이터 전달, share button modal 및 axios 데이터 전달
    1. star button
     - 임시로 toggle로 카운트 기능 추가
    
    2. follow button
     - 임시 toggle로 상태 변경 추가
    
    3. save 버튼 axios 데이터 전달
     - collection/toggle 모의로 데이터 전달
    
    4. share 버튼 modal 로 링크 정보 띄우기 데이터 전달
     -  post 번호를 전달하고 modal에 링크 정보 추가
    
    #16

--- src/components/common/SideMenu/ContentsSideMenu/index.tsx
+++ src/components/common/SideMenu/ContentsSideMenu/index.tsx
@@ -1,17 +1,65 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import './style/ContentsSideMenu.scss'
 import Star from '../../../../img/yellowstar.png'
 import FileSave from '../../../../img/filesave.png'
 import Share from '../../../../img/shareicon.png'
 import SideMenuItem from '../Item'
+import { useLocation, useParams} from 'react-router-dom';
+import Modal from '../ShareModal/ShareModal'
+import { useCallback } from 'react'
 
-const ContentsSideMenu = () => {
+const ContentsSideMenu = (props) => {
+    const SERVER_IP = process.env.REACT_APP_BACKEND_HOST
+    //Star버튼
+    const [Liked,setLiked] = useState(0)
+
+    //Follow버튼
+    const [Followed, setFollowed] = useState(true)
+
+    const FollowTrueFalse = () => setFollowed(!Followed)
+    
+    //Share버튼
+    const location = useLocation()
+    
+    const {id} = useParams<{id : string }>()
+    
+    const handleOnShare = useCallback(() => {
+        console.log("포스트 정보 : " ,id);
+        openModal();
+    },[])
+    
+    //Share Modal기능
+    const [modalOpen, setModalOpen] = useState(false)
+     
+      const openModal = () =>{
+          setModalOpen(true)
+      }
+
+      const closeModal = () =>{
+          setModalOpen(false)
+      }
+
+    //Save버튼
+    const axios = require('axios');      
+    
+      const handleOnSave = useCallback(() => {
+          console.log("포스트 정보 : " ,id);
+          axios.post(`${SERVER_IP}/api/v1/collections/toggle/${id}`);
+      },[])
+
+
+
+
     return (
         <div className = "ContentsSideMenu">
-            <SideMenuItem content = {0} image = {Star} onAction={()=>console.log("Star")}/>
-            <SideMenuItem content = {"팔로우"} onAction={()=>console.log("Follow")}/>
-            <SideMenuItem content = {"Save"} image = {FileSave} onAction={()=>console.log("Save")}/>
-            <SideMenuItem content = {"Share"} image = {Share} onAction={()=>console.log("Share")}/>
+            <button onClick ={() => setLiked(Liked + 1)}><SideMenuItem content = {Liked} image = {Star} onAction={()=>console.log("Star")}/></button>
+            <button onClick ={FollowTrueFalse}><SideMenuItem content = {Followed ? 'Follow' : 'Following'} onAction={()=>console.log("Follow")}/></button>
+            <button onClick ={handleOnSave}><SideMenuItem content = {"Save"} image = {FileSave} /></button>
+            <button onClick ={handleOnShare}><SideMenuItem content = {"Share"} image = {Share} /></button>
+            <Modal open ={modalOpen} close = {closeModal} header="Tora Share">
+                공유하시겠습니까?<br />
+                <a href={`http://localhost:3000` + location.pathname}>{`http://localhost:3000` + location.pathname}</a>
+            </Modal>
         </div>
     )
 }
