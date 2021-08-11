961f6e5396f3893398bf021b09e1a3393f6d8051
Nokda <yh1483@naver.com>
2021-08-11 22:40:23 +0900

    # refactor : Modal Title 추가 및 SideMenu 구조 조정
    1. Modal Title 추가
     - Title이 존재시 Title이 추가, 없으면 Title없는 Modal이 생성됨
     - Title design은 임시상태 (추후 design 변경)
    
    2. SideMenu 구조 조정
     - 보기쉽게 각 코드 위치 조정
     - onAction, page 부분을 onAction을 통일
     - rendering 부분에서 불필요한 부분을 제거하고 적절한 위치에 params를 대입
    
    #16

--- src/components/common/Modal/modal.tsx
+++ src/components/common/Modal/modal.tsx
@@ -1,37 +1,34 @@
 import {FunctionComponent} from 'react'
 import Modal from 'react-modal'
-
+import './style/Modal.scss'
 const CustomModal:FunctionComponent<any> = (Props) => {
-    const {modalOpen, modalClose} = Props
+    const {modalOpen, modalClose,title} = Props
 
     // https://reactcommunity.org/react-modal 참고
     const customstyle = {
         content : {
             display: 'flex',
+            flexDirection: 'column',
             justifyContent:'center',
             top: '0',
             bottom: '0',
             left: '0',
             right: '0',
             margin: 'auto',
-            padding:'auto',
+            padding:'3px',
             backgroundColor: '#fefefe',
             boxSizing: 'border-box',
             width: 'max-content',
             height: 'max-content'
         },
-
         overlay: {zIndex: 999}
-    }
-
-    const contentstyle = {
-
     }
 
     const buttonstyle = {
         color: 'black',
         display: 'block',
         position: 'absolute' as 'absolute',
+        top: '-3px',
         right: '10px',
         cursor: 'pointer',
         fontSize: '25px'
@@ -43,13 +40,14 @@ const CustomModal:FunctionComponent<any> = (Props) => {
         isOpen={modalOpen}
         onRequestClose={modalClose}
         style={customstyle}>
-            <div className="modal-contents" style={contentstyle}>
-                    {Props.children}
-            </div>
-            <div className="modal-close">
+            <div className="modal-title">
+                {title && <span className="title"><p>{title}</p></span>}
                 <span className="close" onClick={modalClose} style={buttonstyle}>
                     &times;
                 </span>
+            </div>
+            <div className="modal-contents">
+                    {Props.children}
             </div>
         </Modal>
     )
--- src/components/common/SideMenu/index.tsx
+++ src/components/common/SideMenu/index.tsx
@@ -2,46 +2,32 @@ import React, {useState} from 'react';
 import './style/SideMenu.scss'
 import {history} from '../../../configureStore'
 import Modify from '../../../img/modify.png'
-import Tora from '../../../img/toralogobutton.png'
-import Chat from '../../../img/chatbutton.png'
-import SideMenuItem from './Item';
+import SideMenuItem from './Item'
 import Custom from './Custom'
 
 const SideMenu = (props) => {
-    // const {token} = props.match.params
-    const  [showCon, setShowCon] = useState(false)
-
-    const openPage = (page) => { history.push(page) }
-
     const initData = () => {
         return Array(
-            {key : 0, content : "자유게시판", image : 'https://image.shutterstock.com/image-photo/fresh-red-apple-isolated-on-260nw-254938384.jpg', onAction : "자유게시판", page : '/post'},
-            {key : 1, content : "블로그", image : 'https://image.shutterstock.com/image-vector/abstract-orange-icon-vector-flat-260nw-1104205634.jpg', onAction : "블로그", page : '/blog'},
-            {key : 2, content : "라이센스", image : 'https://image.shutterstock.com/image-photo/blueberries-isolated-260nw-722035450.jpg', onAction : "라이센스", page : '/license'},
-            {key : 3, content : "노트", image : 'https://en.pimg.jp/050/595/319/1/50595319.jpg',onAction : "노트", page: '/post'},
-            {key : 4, image : 'https://en.pimg.jp/050/595/319/1/50595319.jpg', onAction : "채팅", page : '/chat'},
+            {key : 0, content : "자유게시판", image : 'https://image.shutterstock.com/image-photo/fresh-red-apple-isolated-on-260nw-254938384.jpg', onAction : ()=>openPage('/post')},
+            {key : 1, content : "블로그", image : 'https://image.shutterstock.com/image-vector/abstract-orange-icon-vector-flat-260nw-1104205634.jpg', onAction : ()=>openPage('/blog')},
+            {key : 2, content : "라이센스", image : 'https://image.shutterstock.com/image-photo/blueberries-isolated-260nw-722035450.jpg', onAction : ()=>openPage('/license')},
+            {key : 3, content : "노트", image : 'https://en.pimg.jp/050/595/319/1/50595319.jpg',onAction : ()=>openPage('/post')},
+            {key : 4, image : 'https://en.pimg.jp/050/595/319/1/50595319.jpg', onAction : ()=>openPage('/chat')},
         )
     }
-    const [SideList, setSideList] = useState(initData) 
 
+    const  [showCon, setShowCon] = useState(false)
+    const openPage = (page) => { history.push(page) }
+    const [SideList, setSideList] = useState(initData) 
     
     return (
         <div className = "SideMenu">
             <div className = "SideMenu-modify">
                 <button onClick ={() => setShowCon(!showCon)}><img src = {Modify}/></button>
             </div>
-            <>
-                {
-                    SideList.map(item => {
-                        return (
-                            <button onClick={ ()=>openPage(item.page)}>
-                                <SideMenuItem key = {item.key} content = {item.content} image = {item.image} onAction={()=>console.log(item.onAction)}/>
-                            </button>
-                        )
-                    })
-               
-                }
-            </>
+
+            { SideList.map(item => <SideMenuItem content = {item.content} image = {item.image} onAction={item.onAction}/>) }
+
             <div className = "SideMenu-custom">
                 {showCon && <Custom/>}
             </div>
