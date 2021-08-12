366193e3306cdc64f387e414b1868ef743f0f72a
taewook1 <aktw96@naver.com>
2021-08-13 01:06:39 +0900

    # repator  :  star button toggle기능 추가,  share button modal 및 현재 페이지 띄우기, ide버튼 페이지 연결
    1. star button
     - 임시로 toggle로 카운트 기능 추가
    
    2. share 버튼 modal 로 링크 정보 띄우기 데이터 전달
     -  post 번호를 전달하고 modal에 링크 정보 추가
     - modal은 기존 modal 컴포넌트 사용
     - 현재 페이지 주소를 가져오는 메소드로 대체
    
     5. IDE버튼  클릭시 IDE페이지로 이동
     - history 사용
    
     #16

--- src/components/common/SideMenu/ProjectSideMenu/index.tsx
+++ src/components/common/SideMenu/ProjectSideMenu/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './style/ProjectSideMenu.scss'
 import Star from '../../../../img/yellowstar.png'
 import Share from '../../../../img/shareicon.png'
@@ -6,14 +6,46 @@ import Fork from '../../../../img/forkblack.png'
 import IDE from '../../../../img/toralogobutton.png'
 import Chat from '../../../../img/communication80.png'
 import SideMenuItem from '../Item'
+import {history} from '../../../../configureStore'
+import CustomModal from '../../Modal/modal';
+import { useCallback } from 'react'
 
-const ProjectSideMenu = () => {
+
+const ProjectSideMenu = (props) => {
+
+    const openPage = (url) => { history.push(url) }
+
+    //Star버튼 
+    const [Liked,setLiked] = useState(0)
+
+    //Share버튼
+    const {id} =props;
+    
+    const handleOnShare = useCallback(() => {
+        console.log("프로젝트 정보 : " ,id);
+        openModal();
+    },[])
+    //Share Modal기능
+    const [modalOpen, setModalOpen] = useState(false)
+    
+        const openModal = () =>{
+            setModalOpen(true)
+        }
+
+        const closeModal = () =>{
+            setModalOpen(false)
+        }
+
     return (
         <div className = "ProjectSideMenu">
-            <SideMenuItem content = {0} image = {Star} onAction={()=>console.log("like")}/>
-            <SideMenuItem content = {"Share"} image = {Share} onAction={()=>console.log("Share")}/>
+            <button className="like"><SideMenuItem content = {Liked} image = {Star} onAction={()=>setLiked(Liked +1)}/></button>
+            <button><SideMenuItem content = {"Share"} image = {Share} onAction={handleOnShare}/></button>
+            <CustomModal modalOpen ={modalOpen} modalClose = {closeModal} header="Tora Share">
+                공유하시겠습니까?<br />
+                <a href={window.location.href}>{window.location.href}</a>
+            </CustomModal>
             <SideMenuItem content = {"Fork"} image = {Fork} onAction={()=>console.log("Fork")}/>
-            <SideMenuItem content = {"IDE"} image = {IDE} onAction={()=>console.log("IDE")}/>
+            <button><SideMenuItem content = {"IDE"} image = {IDE} onAction={()=>openPage('/IDE')}/></button>
             <SideMenuItem content = {"Chat"} image = {Chat} onAction={()=>console.log("Chat")}/>
         </div>
     )
