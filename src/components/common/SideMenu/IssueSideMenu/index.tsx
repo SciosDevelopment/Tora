4dd354b7a2c4ac2a438785582189b640a9d5d276
taewook1 <aktw96@naver.com>
2021-08-13 01:03:56 +0900

    # repator  :  star button toggle기능 추가, follow button toggle 기능 추가, save 버튼 데이터 전송하도록 추가 , share button modal 및 현재 페이지 띄우기, close버튼 toggle 형식추가
    1. star button
     - 임시로 toggle로 카운트 기능 추가
    
    2. follow button
     - 임시 toggle로 상태 변경 추가
    
    3. save 버튼 axios 데이터 전달
     - collection/toggle 모의로 데이터 전달
    
    4. share 버튼 modal 로 링크 정보 띄우기 데이터 전달
     -  post 번호를 전달하고 modal에 링크 정보 추가
     - modal은 기존 modal 컴포넌트 사용
     - 현재 페이지 주소를 가져오는 메소드로 대체
    
     5. open/close 버튼 toggle 방식으로 추가
     - open 이미지가 없어서 파일 save이미지로 대체 해놓음

--- src/components/common/SideMenu/IssueSideMenu/index.tsx
+++ src/components/common/SideMenu/IssueSideMenu/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './style/IssueSideMenu.scss'
 import Star from '../../../../img/yellowstar.png'
 import Share from '../../../../img/shareicon.png'
@@ -6,15 +6,55 @@ import Save from '../../../../img/filesave.png'
 import UPArrow from '../../../../img/uparrow.png'
 import Close from '../../../../img/closeicon.png'
 import SideMenuItem from '../Item'
+import CustomModal from '../../Modal/modal';
+import { useCallback } from 'react'
 
-const IssueSideMenu = () => {
+const IssueSideMenu = (props) => {
+
+    //Star 버튼
+    const [Liked,setLiked] = useState(0)
+
+    //Issue openclose toggle버튼
+    const [Opened, setOpened] = useState(false)
+
+    const OpenClosed = () => setOpened(!Opened)
+
+    //Share버튼
+    const {id} =props;
+
+    const handleOnShare = useCallback(() => {
+        console.log("이슈 정보 : " ,id);
+        openModal();
+    },[])
+    //Share modal
+    const [modalOpen, setModalOpen] = useState(false)
+     
+      const openModal = () =>{
+          setModalOpen(true)
+      }
+
+      const closeModal = () =>{
+          setModalOpen(false)
+      }
+
+      const axios = require('axios');   
+
+    const handleOnSave = useCallback(() => {
+        console.log("포스트 정보 : " ,id);
+        axios.post(`http://localhost:3000/api/v1/collections/toggle`);
+    },[])
+
     return (
         <div className = "IssueSideMenu">
-            <SideMenuItem content = {0} image = {Star} onAction={()=>console.log("like")}/>
-            <SideMenuItem content = {"Save"} image = {Save} onAction={()=>console.log("Save")}/>
-            <SideMenuItem content = {"Share"} image = {Share} onAction={()=>console.log("Share")}/>
+            <button className="like"><SideMenuItem content = {Liked} image = {Star} onAction={()=>setLiked(Liked + 1)}/></button>
+            <button><SideMenuItem content = {"Save"} image = {Save} onAction={handleOnSave}/></button>
+            <button><SideMenuItem content = {"Share"} image = {Share} onAction={handleOnShare}/></button>
+            <CustomModal modalOpen ={modalOpen} modalClose = {closeModal} header="Tora Share">
+                공유하시겠습니까?<br />
+                <a href={window.location.href}>{window.location.href}</a>
+            </CustomModal>
             <SideMenuItem content = {"Upload"} image = {UPArrow} onAction={()=>console.log("Upload")}/>
-            <SideMenuItem content = {"Close"} image = {Close} onAction={()=>console.log("Close")}/>
+            <button><SideMenuItem content = {Opened ? 'Open' : 'Close'} image = {Opened ? Save : Close } onAction={OpenClosed}/></button>
         </div>
     )
 }
