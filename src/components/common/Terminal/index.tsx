2a68c0bb9a4a3f84564d98d05d98dcfa63395a18
Nokda <yh1483@naver.com>
2021-08-17 16:00:05 +0900

    # refactor : KeyEvent 구조 변경
    - onKey에서 onData로 변경 (IME 설정)
    - 코드 이해하기 쉽게 각 KeyCode마다 작동하는 함수 생성
    
    #21

--- src/components/common/Terminal/index.tsx
+++ src/components/common/Terminal/index.tsx
@@ -17,6 +17,7 @@ const TerminalView  = (props) => {
     const {terminalID, userInfo = "userInfo"} = props
     var term : Terminal
     var cur_line = ''
+    var cursor = 0
     const SERVER_IP = process.env.REACT_APP_BACKEND_HOST
     const welcomeText = "this is tora command\n\rthis cmd version is 0.0.1\n\n\n\n\n\n\n\n\n\n\n\n"
     useEffect(() => { init() }, [])
@@ -31,6 +32,13 @@ const TerminalView  = (props) => {
         }
 
     }
+    const termBackspace = () => {
+        if (cur_line.length > 0) {  // 한글에 대해서는 처리가 되지않은 상태
+            // if deletable text has, delete text.
+            cur_line = cur_line.slice(0, -1)
+            term.write('\b \b')
+        }
+    }
 
     const init = async() => {
         var terminalView = document.getElementById('Terminal-main')
@@ -41,7 +49,7 @@ const TerminalView  = (props) => {
         terminalView.appendChild(terminal)
         
         if(terminal == null) return // terminal is not activated. (error)
-        var ws = new WebSocket('ws://localhost:3001/');
+        var ws = new WebSocket('ws://localhost:9898/')
         term = new Terminal(options)
         term.open(terminal)
 
@@ -55,27 +63,19 @@ const TerminalView  = (props) => {
     const InitUISetting = async() => {
         term.write(welcomeText)
         termEnter()
-        
-        term.onKey((key) => {
-            var ev = key.domEvent
-            var printable = (!ev.altKey && /* !ev.altGraphKey && */ !ev.ctrlKey && !ev.metaKey)
 
-            if (ev.key === "Enter") termEnter()
-            
-            // 추후 개발 : tab, Cancel, arrow
-            else if (ev.key === "Tab") { // tab : autoKeyword
-            } else if (ev.ctrlKey) { // ctrl == ^
-                if(ev.key === "c" || ev.key ==="C") console.log("Cancel") //^C
-                if(ev.key === "v") console.log("ctrl+v") //^V
-            } else if (ev.key === "Backspace") {
-                if (cur_line.length > 0) { 
-                    // if deletable text has, delete text.
-                    cur_line = cur_line.slice(0, -1)
-                    term.write('\b \b')
-                }
-            } else if (printable) {
-                term.write(ev.key)
-                cur_line += ev.key
+        term.onData((data) => {
+            // 참고자료 : https://www.linkedin.com/pulse/xtermjs-local-echo-ioannis-charalampidis/
+            const code_ = data.charCodeAt(0)
+
+            if(code_ > 127) return // 한글입력 임시차단
+
+            if (code_ == 13) termEnter() // Enter
+            else if(code_ == 127) termBackspace() // backspace
+            else if (code_ < 32) return// Control  
+            else { // Visible
+                term.write(data)
+                cur_line += data
             }
         })
     }
@@ -86,3 +86,5 @@ export default TerminalView
 // xterm example : https://codepen.io/ScreaminSauce/pen/ZBRGEE
 // node-pty : Writing a terminal emulater (eg. via xterm.js) : https://github.com/microsoft/node-pty
 // node-pty + ws + xterm example : https://blog.totaljs.com/posts/140289001cx61b/
+
+// wetty (Web + tty) 참고자료 : https://github.com/butlerx/wetty
