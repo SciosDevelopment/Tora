728fad1b7dc2db46c29e83f96403ef3a81b84f02
Nokda <yh1483@naver.com>
2021-08-23 01:05:27 +0900

    #feat : "댓글이 없습니다" 컴포넌트 구현
    - 댓글이 없는 경우, 댓글이 없습니다 컴포넌트가 뜨도록 추가
    
    #17

--- src/components/common/CommentList/index.tsx
+++ src/components/common/CommentList/index.tsx
@@ -8,8 +8,8 @@ const CommentList = (props) => {
     const [comment_id, setCommentID] = useState(-1)
     const [mode, setMode] = useState("reply")
     const [pc, setPC] = useState("")
+    
     // CommentList 컴포넌트 List 구현 필요
-
     const setCommentID_ = (comment_num, mode, pc = "")=> {
         setMode(mode)
         if(comment_id === comment_num) setCommentID(-1)
@@ -21,7 +21,7 @@ const CommentList = (props) => {
 
     return (
         <>
-            {list.map((data_)=><Comment data={data_} onClick={setCommentID_}/>)} 
+            {list.length === 0 ? <div className="Comment-NotExist">"댓글이 존재하지 않습니다"</div> : list.map((data_)=><Comment data={data_} onClick={setCommentID_}/>)} 
             <CommentPost comment_id={comment_id} post_id={post_id} post_content = {pc} mode={mode} Cancel={cancel}/>
             <input type="button" value="[reply]" onClick={()=>setCommentID_(post_id, "reply")}/>
         </>
