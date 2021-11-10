525cd146c8255659715a868bfc4b8b08a46dd841
Nokda <yh1483@naver.com>
2021-11-10 15:15:51 +0900

    # feat : DropDown Edit 기능 구현
    - 되돌리기, 다시실행, 라인 주석추가/제거, 블럭 주석추가/제거, 찾기, 찾아바꾸기 구현
    - 잘라내기, 복사, 붙여넣기 중 붙여넣기가 chrome에서 구현할 수 없는 상태여서 3가지 모두 제외
    - 찾기, 찾아바꾸기 부분은 독자적인 UI로 변경할 예정 (추후)
    
    #27

--- src/pages/IDE/Header/Dropdown/Edit.tsx
+++ src/pages/IDE/Header/Dropdown/Edit.tsx
@@ -2,24 +2,54 @@ import Dropdown from "."
 import {target} from '../../../../components/common/TextEditor'
 
 const EditDropdown = () => {
-    
+    // vscode-keybindings : https://vscode-docs.readthedocs.io/en/stable/customization/keybindings/
     // getactions map : target.current.editor.getActions().map(a => a.id)
     // const testfunc = ()=> {if(target!=null)target.current.editor.getActions().map(a => console.log(a.id))}    
     // example 
-    const testfunc = ()=> {
-        if (target!=null) target.current.editor.getAction("editor.action.showContextMenu").run()
+
+    // 되돌리기
+    const func_undo = () => {
+        if (target==null) return
+        target.current.editor.trigger('-','undo')
+    }
+
+    // 다시실행
+    const func_redo = () => {
+        if (target==null) return
+        target.current.editor.trigger('-','redo')
     }
     
-    const EditDrop = Array(
-        {title : "뒤로가기", shortcut : null, onAction : testfunc},
-        {title : "다시실행", shortcut : null, onAction : testfunc},
-        {title : "잘라내기", shortcut : null, onAction : testfunc},
-        {title : "복사", shortcut : null, onAction : testfunc},
-        {title : "붙여넣기", shortcut : null, onAction : testfunc},
-        {title : "북마크", shortcut : null, onAction : testfunc},
-        {title : "주석추가 / 제거", shortcut : null, onAction : testfunc},
-        {title : "찾기", shortcut : null, onAction : testfunc},
-        {title : "치환", shortcut : null, onAction : testfunc})
+    // 주석 추가 / 제거
+    const func_toggle_comment = () => {
+        if (target==null) return
+        target.current.editor.getAction("editor.action.commentLine").run()
+    }
+
+    const func_toggle_comment2 = () => {
+        if (target==null) return
+        target.current.editor.getAction("editor.action.blockComment").run()
+    }
+
+    // 찾기
+    const func_find = () => {
+        if (target == null) return
+        target.current.editor.getAction("actions.find").run()
+    }
+
+    // 찾아 바꾸기
+    const func_replace = () => {
+        if (target==null) return
+        target.current.editor.getAction("editor.action.startFindReplaceAction").run()
+    }
+
+    const EditDrop = [
+        {title : "되돌리기", shortcut : null, onAction : func_undo},
+        {title : "다시실행", shortcut : null, onAction : func_redo},
+        {title : "라인 주석추가 / 제거", shortcut : null, onAction : func_toggle_comment},
+        {title : "블럭 주석추가 / 제거", shortcut : null, onAction : func_toggle_comment2},
+        {title : "찾기", shortcut : null, onAction : func_find},
+        {title : "찾아바꾸기", shortcut : null, onAction : func_replace}
+    ]
 
     return <Dropdown title="Edit" data={EditDrop}/> // render 
 }
