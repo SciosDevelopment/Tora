de5c50149a5006ec8dcde54edb9cfac2eeb1474d
Nokda <yh1483@naver.com>
2021-12-12 02:09:18 +0900

    # refactor : License css 및 불필요한 코드 정리 - 각 layout 부분의 css 파일 제거 후 scss main으로 통합 - 불필요하고 중복되는 의미의 css, variable naming 제거

--- src/pages/License/index.tsx
+++ src/pages/License/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import LicenseView from './LicenseView/LicenseView'
+import LicenseView from './LicenseView'
 import Header from '../../components/common/Header/Header'
-import './style/LicenseMain.scss'
+
 const LicenseMain = () => {
     return (
-        <div className = "License-main-container">
+        <div className = "licensePage">
             <Header/>
             <LicenseView/>   
         </div>
