8ff8bc04d1448281cfa82a233f1a1f85b73f1700
Nokda <yh1483@naver.com>
2021-12-12 01:32:10 +0900

    # refactor : Business css 및 불필요한 코드 정리
    - 각 layout 부분의 css 파일 제거 후 scss main으로 통합
    - 불필요하고 중복되는 의미의 css, variable naming 제거

--- src/pages/Business/index.tsx
+++ src/pages/Business/index.tsx
@@ -1,78 +1,77 @@
-import React from 'react';
-import './style/businessmain.scss'
+import React from 'react'
 
-const businessmain = () => {
+const BusinessMain = () => {
     return (
-        <div className = "Business-main">
-            <div className = "Business-title">
+        <div className = "business">
+            <div className = "title">
                 <p>Service Plan Introduction Inquiries</p>
             </div>
-            <div className = "Business-view">
-            <div className = "business-view-main">
-            <div className = "business-view-free">
-                <div className = "business-view-free-title">
-                    <p>FREE</p>
-                </div>
-                <div className = "business-view-free-price">
-                    <p>$ 0</p>
-                </div>
-                <div className = "business-view-free-term">
-                    <p>per user/month</p>
-                </div>
-                <div className = "business-view-free-text">
-                    <p>500MB 제공</p>
-                    <p>화상코딩 50명까지</p>
-                </div>
-                <div className = "business-view-free-button">
-                <input type='submit' value="Join for Free"></input>
-                </div>
-            </div>
-            <div className = "business-view-premium">
-                <div className = "business-view-premium-title">
-                    <p>PREMIUM</p>
-                </div>
-                <div className = "business-view-premium-price">
-                    <p>$ 3</p>
-                </div>
-                <div className = "business-view-premium-term">
-                    <p>per user/month</p>
-                </div>
-                <div className = "business-view-premium-text">
-                    <p>5GB 제공</p>
-                    <p>화상코딩 무제한</p>
-                    <p>프리미엄 앱 서버</p>
-                    <p>광고 제거</p>
-                </div>
-                <div className = "business-view-premium-button">
-                    <input type='submit' value="30 days of free experience"></input>
-                </div>
-            </div>
-            <div className = "business-view-enterprise">
-                <div className = "business-view-enterprise-title">
-                    <p>ENTERPRISE</p>
-                </div>
-                <div className = "business-view-enterprise-price">
-                    <p>$ 10</p>
-                </div>
-                <div className = "business-view-enterprise-term">
-                    <p>per user/month</p>
-                </div>
-                <div className = "business-view-enterprise-text">
-                    <p>10GB 제공</p>
-                    <p>화상코딩 무제한</p>
-                    <p>프리미엄 앱 서버</p>
-                    <p>업무 캘린더</p>
-                    <p>라이센스 서비스</p>
-                    <p>모니터링</p>
-                </div>
-                <div className = "business-view-enterprise-button">
-                    <input type='submit' value="30 days of free experience"></input>
+            <div className = "content">
+                <div className = "container">
+                    <div className = "free">
+                        <div className = "title">
+                            <p>FREE</p>
+                        </div>
+                        <div className = "price">
+                            <p>$ 0</p>
+                        </div>
+                        <div className = "term">
+                            <p>per user/month</p>
+                        </div>
+                        <div className = "text">
+                            <p>500MB 제공</p>
+                            <p>화상코딩 50명까지</p>
+                        </div>
+                        <div className = "button">
+                            <input type='submit' value="Join for Free"/>
+                        </div>
+                    </div>
+                    <div className = "premium">
+                        <div className = "title">
+                            <p>PREMIUM</p>
+                        </div>
+                        <div className = "price">
+                            <p>$ 3</p>
+                        </div>
+                        <div className = "term">
+                            <p>per user/month</p>
+                        </div>
+                        <div className = "text">
+                            <p>5GB 제공</p>
+                            <p>화상코딩 무제한</p>
+                            <p>프리미엄 앱 서버</p>
+                            <p>광고 제거</p>
+                        </div>
+                        <div className = "button">
+                            <input type='submit' value="30 days of free experience"/>
+                        </div>
+                    </div>
+                    <div className = "enterprise">
+                        <div className = "title">
+                            <p>ENTERPRISE</p>
+                        </div>
+                        <div className = "price">
+                            <p>$ 10</p>
+                        </div>
+                        <div className = "term">
+                            <p>per user/month</p>
+                        </div>
+                        <div className = "text">
+                            <p>10GB 제공</p>
+                            <p>화상코딩 무제한</p>
+                            <p>프리미엄 앱 서버</p>
+                            <p>업무 캘린더</p>
+                            <p>라이센스 서비스</p>
+                            <p>모니터링</p>
+                        </div>
+                        <div className = "button">
+                            <input type='submit' value="30 days of free experience"/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-            </div>
-        </div>
-    );
-};
+    )
+}
 
-export default businessmain;
+export default BusinessMain
